51a4c1573cd061fc3ec718c080142a3bed849030
Muhammad Moeen <mooenmalik374@gmail.com>
2025-12-14 00:17:21 +0500

    feat: implemented Clerk webhook handling for user creation, update, and deletion with database integration

--- app/api/clerk/route.js
+++ app/api/clerk/route.js
@@ -2,13 +2,40 @@ import { Webhook } from "svix";
 import connectToDB from "@/config/db";
 import User from "@/models/User";
 import { headers } from "next/headers";
+import { NextRequest } from "next/server";
 
-export async function POST(req){
-    const wh = new Webhook(process.env.SIGNIN_SECRET);
-    const headerPayload = await headers();
-    const svixHeaders={
-        "svix-id":headerPayload.get("svix-id"),
-        "svix-timestamp":headerPayload.get("svix-timestamp"),
-        "svix-signature":headerPayload.get("svix-signature"),
-    }
+export async function POST(req) {
+  const wh = new Webhook(process.env.SIGNIN_SECRET);
+  const headerPayload = await headers();
+  const svixHeaders = {
+    "svix-id": headerPayload.get("svix-id"),
+    "svix-timestamp": headerPayload.get("svix-timestamp"),
+    "svix-signature": headerPayload.get("svix-signature"),
+  };
+  // ? getting payload and its verification
+  const payload = await req.json();
+  const body = JSON.stringify(payload);
+  const { data, type } = wh.verify(body, svixHeaders);
+  //? user data preparation to store in DB
+  const userData = {
+    _id: data.id,
+    email: data.email_addresses[0].email_address,
+    name: `${data.first_name} ${data.last_name}`,
+    image: data.image_url,
+  };
+  await connectToDB();
+  switch (type) {
+    case "user.created":
+      await User.create(userData);
+      break;
+
+    case "user.updated":
+      await User.findByIdAndUpdate(data.id, userData);
+      break;
+
+    case "user.deleted":
+      await User.findByIdAndDelete(data.id);
+      break;
+  }
+  return NextRequest.json({ message: "Clerk Webhook received successfully" });
 }
--- config/db.js
+++ config/db.js
@@ -1,5 +1,6 @@
 import mongoose from "mongoose";
 
+// ! The entire file is quite important as it not only configures the database connection but also checks whether the connection is already established or in the process of being established. This caching mechanism is crucial for performance optimization, especially in serverless environments where functions may be invoked multiple times in quick succession.
 let cached = global.mongoose || { conn: null, promise: null };
 export default async function connectToDB() {
   if (cached.conn) return cached.conn;
